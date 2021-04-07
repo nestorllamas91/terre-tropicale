7f1b0c50ade4ab43ed97e7993c39f22289f56e60
Néstor Llamas <nestorllamas91@gmail.com>
2021-04-07 15:32:21 +0200

    fix: modifications in the structure of the application

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,4 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  clearMocks: true,
-  collectCoverage: true,
-  coverageReporters: [['lcov']],
-  coveragePathIgnorePatterns: ['node_modules'],
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-    },
-  },
-  reporters: ['default'],
-  restoreMocks: true,
-  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
 };
