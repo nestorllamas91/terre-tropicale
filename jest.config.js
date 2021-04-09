5ee74b7b915b5cf12393ce85ef06e6d8f8dc9ed1
Néstor Llamas <nestorllamas91@gmail.com>
2021-04-09 19:56:57 +0200

    chore: modifications in the structure of the application

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,16 @@
 module.exports = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
-  moduleDirectories: ['node_modules', 'src']
+  moduleDirectories: ['node_modules', 'src'],
+  moduleNameMapper: {
+    '@/config/(.*)': '<rootDir>/src/config/$1',
+    '@/app/accueil/(.*)': '<rootDir>/src/app/accueil/$1',
+    '@/app/nos-smoothies/(.*)': '<rootDir>/src/app/nos-smoothies/$1',
+    '@/app/nos-cocktails/(.*)': '<rootDir>/src/app/nos-cocktails/$1',
+    '@/app/a-propos/(.*)': '<rootDir>/src/app/a-propos/$1',
+    '@/app/contact/(.*)': '<rootDir>/src/app/contact/$1',
+    '@/app/error-404/(.*)': '<rootDir>/src/app/error-404/$1',
+    '@/app/error-500/(.*)': '<rootDir>/src/app/error-500/$1',
+    '@/app/shared/(.*)': '<rootDir>/src/app/shared/$1'
+  }
 };
