467b2b4285691f97e1fa35a7ee911cd79170b7ff
Néstor Llamas <nestorllamas91@gmail.com>
2021-04-15 12:17:04 +0200

    feat: add page "a propos"

--- src/app/a-propos/component.tsx
+++ src/app/a-propos/component.tsx
@@ -1,18 +1,51 @@
 import Head from 'next/head';
 
+import { ValeursSection } from '@/app/accueil/component';
 import Layout from '@/app/shared/layout/component';
 
-const APropos = (): JSX.Element => {
+const AProposPage = (): JSX.Element => {
   return (
-    <div>
+    <>
       <Head>
         <title>À propos | Terre Tropicale</title>
       </Head>
       <Layout>
-        <span>À propos</span>
+        <HeaderSection />
+        <QuiSommesNousSection />
+        <ValeursSection />
       </Layout>
-    </div>
+    </>
   );
 };
 
-export default APropos;
+export default AProposPage;
+
+const HeaderSection = () => (
+  <header className="relative flex mb-6">
+    <img src="/assets/images/a-propos/header.jpg" />
+    <div className="absolute flex flex-col justify-center px-4 w-full h-full text-white bg-black bg-opacity-40">
+      <h1 className="mb-2">À PROPOS</h1>
+      <p>Découvrez des gouts authentiques venus des mythiques cordillères des Andes</p>
+    </div>
+  </header>
+);
+
+const QuiSommesNousSection = () => (
+  <div className="mb-6">
+    <h1 className="mb-2 text-center">QUI SOMMES-NOUS?</h1>
+    <div className="px-4">
+      <p>
+        Terre Tropicale est née en 2011 de la volonté de partager la culture et les saveurs des produits tropicaux en
+        provenance d&apos;Amérique Latine. Nous nous spécialisons dans la distribution de pulpe des fruits, un produit
+        aux saveurs authentiques et originales, fair de vrais fruits.
+      </p>
+      <p>
+        Nous apportons aux professionels de la restauration une offre de proximité et de diversité en terms de goûts et
+        de qualité. Nous leur offrons, une large gamme de saveurs tropicales uniques à découvrir, disponibles toute
+        l&apos;année.
+      </p>
+      <p>Laissez-vous tenter, dégustez et savourez!</p>
+    </div>
+    <img src="/assets/images/a-propos/qui-sommes-nous.jpg" />
+  </div>
+);
