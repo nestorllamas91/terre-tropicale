8cea398d4d5020f45f120d0a9a3dbbaa2f23c67d
Néstor Llamas <nestorllamas91@gmail.com>
2021-06-09 01:24:18 +0200

    fix: fix some things

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,15 +7,13 @@ import { appWithTranslation } from 'next-i18next';
 
 import Maintenance from '@/maintenance-page/component';
 
-console.log(!!process.env.NEXT_PUBLIC_MAINTENANCE_MODE);
-console.log(typeof !!process.env.NEXT_PUBLIC_MAINTENANCE_MODE);
 const App = ({ Component, pageProps }: AppProps): JSX.Element => (
   <>
     <Head>
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
     </Head>
-    {!!process.env.NEXT_PUBLIC_MAINTENANCE_MODE === true ? <Maintenance /> : <Component {...pageProps} />}
+    {process.env.NEXT_PUBLIC_MAINTENANCE_MODE === 'yes' ? <Maintenance /> : <Component {...pageProps} />}
   </>
 );
 
